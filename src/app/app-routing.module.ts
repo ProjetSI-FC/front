f726105847b7ea959e6917708f0daf8a5e89d66b
Théo Souchon <souchontpro@gmail.com>
2023-10-10 09:52:25 +0200

    TSO#1: Empty project with gitignore
    TSO#1 : Init project pages
    
    TSI#1: Init structure project
    
    TSO#1: Refacto
    
    TSO#1 : ..
    
    CGE#1: creating autocomplete/select bar
    
    CGE#1: importing Checkbox Autocomplete module
    
    CGE#1: importing MatInputModule
    
    [CGE][#1]:adding keyword search bar
    
    TSO#1:ressources foler
    
    CGE#1: starting to add svg icon
    
    CGE#1: adding sanitizer
    
    TSO#1:Button clean field with image cross
    
    TSO#1:Style css
    
    Remove all tag with filter button
    
    TSO#1 : Routing pages
    
    TSO#1:up to date
    
    CGE#1: adding magnifying glass button
    
    [CGE][#1] scss search bar + titre
    
    [TSO][#1] : secondary page to result page + remove unused imports
    
    [TSO][#1] : remove unused class keyword
    
    Rebase branch

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ResultPageComponent } from './pages/result-page/result-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'second', component: ResultPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
