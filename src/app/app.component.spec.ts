fd0c05a3c1a9012db7c2c66ddd78bd6dbf527265
Théo Souchon <souchontpro@gmail.com>
2023-10-25 10:36:17 +0200

    TSO#1:ESLint fix files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
+    })
+  )
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(HomePageComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-  
-});
+    const fixture = TestBed.createComponent(HomePageComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SecondaryPageComponent } from './pages/secondary-page/secondary-page.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { SecondaryPageComponent } from './pages/secondary-page/secondary-page.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    SecondaryPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomePageComponent, SecondaryPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
