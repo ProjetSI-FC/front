cc2db06932210b2020baf1b74ab1f7a8d5847614
Théo Souchon <souchontpro@gmail.com>
2024-01-08 12:23:40 +0100

    [TSO][1] : try ignore eslint

--- src/app/shared-component/searchbar/searchbar.component.ts
+++ src/app/shared-component/searchbar/searchbar.component.ts
@@ -6,6 +6,7 @@ import { map, startWith } from 'rxjs/operators'
 import { LiveAnnouncer } from '@angular/cdk/a11y'
 import { type MatChipInputEvent } from '@angular/material/chips'
 import { type MatAutocompleteSelectedEvent } from '@angular/material/autocomplete'
+// eslint-disable-next-line no-use-before-define
 import { MatIconRegistry } from '@angular/material/icon'
 import { DomSanitizer } from '@angular/platform-browser'
 import { Router } from '@angular/router'
